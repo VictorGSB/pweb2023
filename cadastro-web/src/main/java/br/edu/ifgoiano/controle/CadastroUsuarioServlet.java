9c5d0b9f2cafc6c90d8dcc29e3d3194092fe7647
2021108102540090 <2021108102540090@lab01-20.ifgoiano.local>
2023-03-28 10:42:43 -0300

    feito com bootstrap

--- cadastro-web/src/main/java/br/edu/ifgoiano/controle/CadastroUsuarioServlet.java
+++ cadastro-web/src/main/java/br/edu/ifgoiano/controle/CadastroUsuarioServlet.java
@@ -1,6 +1,8 @@
 package br.edu.ifgoiano.controle;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,9 +10,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.util.digester.SystemPropertySource;
+
+import br.edu.ifgoiano.entidade.usuario;
+
 
 @WebServlet ("/cadastrarUsuario")
 public class CadastroUsuarioServlet extends HttpServlet {
+	//Simular o banco de dados 
+	
+	private List<usuario> lstDeUsuarios;
+	
+	@Override
+	public void init() throws ServletException {
+		
+		this.lstDeUsuarios = new ArrayList<usuario>();
+		
+		
+		
+	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,6 +39,15 @@ public class CadastroUsuarioServlet extends HttpServlet {
 		//Verificar se as senhas sao igais
 		
 		if(senha1.equals(senha2)) {
+			
+			usuario usu = new usuario();
+			
+			usu.setNome(req.getParameter("nome"));
+			usu.setEmail(req.getParameter("email"));
+			usu.setSenha(senha1);
+			
+			lstDeUsuarios.add(usu);
+				
 			//Redirecionar o usuario para a pagina de login.
 			
 			resp.sendRedirect("index.html");
@@ -29,9 +56,27 @@ public class CadastroUsuarioServlet extends HttpServlet {
 			
 			//Redirecionar o usuario para a mesma pagina de cadastro de usuario.
 			
+			
+			
 			req.getRequestDispatcher("CadastroUsuario.html").forward(req, resp);
 		}
 		
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		for (usuario usuario : lstDeUsuarios) {
+			
+			System.out.println(usuario.getNome().concat(" - ").concat(usuario.getEmail()));
+			
+		}
+	}
+	
+	@Override
+	public void destroy() {
+		
+		this.lstDeUsuarios.clear();
 	}
 
 }
