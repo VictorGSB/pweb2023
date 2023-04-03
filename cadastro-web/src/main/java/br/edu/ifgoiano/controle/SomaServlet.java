b379bbb191b8af0fab86d66ca0886da6e8527255
Victor Guilherme <119265675+VictorGSB@users.noreply.github.com>
2023-04-03 19:41:48 -0300

    Update SomaServlet.java

--- cadastro-web/src/main/java/br/edu/ifgoiano/controle/SomaServlet.java
+++ cadastro-web/src/main/java/br/edu/ifgoiano/controle/SomaServlet.java
@@ -17,17 +17,16 @@ public class SomaServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		
-		String n1 = req.getParameter("n1");
-		String n2 = req.getParameter("n2");
+		int nun1 = Integer.parseInt(req.getParameter("n1"));
+		int nun2 = Integer.parseInt(req.getParameter("n2"));
 		
-		int nun1 = Integer.parseInt(n1);
-		int nun2 = Integer.parseInt(n2);
 		
-		int soma = nun1 + nun2; 
+		int soma = nun1 + nun2;
+
+			
+		resp.getWriter().println("A soma dos valores é " + soma);
 		
-		PrintWriter somando = resp.getWriter();
-		
-		somando.print(soma);
+		;
 		
 		
 	}
