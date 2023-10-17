a5f3c61172836bedd33c22ab91915f8ffa9fbba5
Victor Guilherme <119265675+VictorGSB@users.noreply.github.com>
2023-10-17 10:41:25 -0300

    API Medico

---
+++ api/src/main/resources/static/medico.js
@@ -0,0 +1,17 @@
+{	"id" : null,
+	"nome": "Victor Guilheme Souza Barros",
+	"email": "victor.guilherme@gmail.com",
+	"crn": 957463,
+	"especialidade" : "CARDIOLOGIA",
+	"endereco": {
+		"id":null
+		"logradouro": "Rua Wilson Reis"
+		"barirro":"Cristina II",
+		"complemento": "Quadra 25 Lote 2",
+		"cep": "95547854",
+		"cidade": "Trindade",
+		"estado": "Goiás"
+	}
+		
+}
+
