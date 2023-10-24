c39ca494d53e4ac3836ba7cbd9ae2cb957880049
Victor Guilherme <119265675+VictorGSB@users.noreply.github.com>
2023-10-24 10:35:29 -0300

    Atualização ad API Medico
    Atualização ad API Medico

--- api/src/main/java/br/edu/ifgoiano/controller/MedicoController.java
+++ api/src/main/java/br/edu/ifgoiano/controller/MedicoController.java
@@ -1,8 +1,11 @@
 package br.edu.ifgoiano.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifgoiano.entity.Medico;
 
 @RestController
 @RequestMapping("/medico")
@@ -11,8 +14,11 @@ public class MedicoController {
 	
 	@PostMapping
 	
-	public void cadastrar(String medico) {
-		System.out.println(medico);
+	public void cadastrar(@RequestBody Medico medico) {
+		System.out.println(medico.getNome());
+
+		
+		
 	}
 
 }
