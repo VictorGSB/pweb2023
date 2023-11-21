5d071bde927ef0d89ab8b3c869365d80a5c884b3
Victor Guilherme <119265675+VictorGSB@users.noreply.github.com>
2023-11-21 10:47:37 -0300

    Add files via upload

--- api/src/main/java/br/edu/ifgoiano/controller/MedicoController.java
+++ api/src/main/java/br/edu/ifgoiano/controller/MedicoController.java
@@ -1,24 +1,41 @@
 package br.edu.ifgoiano.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifgoiano.entity.Medico;
+import br.edu.ifgoiano.repository.MedicoRepository;
 
 @RestController
 @RequestMapping("/medico")
-
 public class MedicoController {
-	
-	@PostMapping
-	
-	public void cadastrar(@RequestBody Medico medico) {
-		System.out.println(medico.getNome());
 
-		
+	@Autowired
+	private MedicoRepository medicoRepository;
+
+	@PostMapping
+	public void cadastrar(@RequestBody Medico medico) {
+		this.medicoRepository.save(medico);
+	}
+	@GetMapping
+	public List<Medico> listar (){
+		return this.medicoRepository.findAll();
 		
 	}
-
+	@PutMapping
+	public void atualizar (@RequestBody Medico medico) {
+		this.medicoRepository.save(medico);
+	}
+	@DeleteMapping("/{id}")
+	public void excluir (@PathVariable Long id) {
+		this.medicoRepository.deleteById(id);
+	}
 }
